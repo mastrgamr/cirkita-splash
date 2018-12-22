737c9b9bf7c7eeafcf81d981f4fb0305836344d8
stuart <themastrgamr@gmail.com>
2018-12-22 16:56:58 -0500

    new info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PrivacyPolicy from './components/PrivacyPolicy'
+import screenshot from './images/tpScreenMock.jpg';
 import './App.css';
+
+import { Button, Container, Divider, Grid, Modal } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Transit Pulse [Beta]</p>
+          <img src={screenshot} className="App-logo" alt="logo" />
+          <p>A transportation app.</p>
+          <Divider />
+          <Container>
+            <Grid stackable divided centered row>
+              <Grid.Row columns={2} textAlign='center'>
+                <Grid.Column width={3}>
+                  <a href="#">
+                    <Button
+                      fluid
+                      animated='fade'>
+                      <Button.Content visible>Download</Button.Content>
+                      <Button.Content hidden>via Google Play Store</Button.Content>
+                    </Button>
+                  </a>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Modal trigger={<Button fluid>Privacy Policy</Button>} closeIcon>
+                    <Modal.Header>Privacy Policy</Modal.Header>
+                    <Modal.Content scrolling>
+                      <Modal.Description>
+                        <PrivacyPolicy />
+                      </Modal.Description>
+                    </Modal.Content>
+                  </Modal>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
         </header>
       </div>
     );
